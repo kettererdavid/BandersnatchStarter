03a118d7acc9a61988d7b08b9a9e0559798621fa
kettererdavid <david.kette@gmail.com>
2023-10-23 10:16:15 -0600

    Sprint2 commit, updated docstring

--- app/graph.py
+++ app/graph.py
@@ -3,6 +3,27 @@ import altair as alt
 
 
 def chart(df, x, y, target) -> Chart:
+    """
+    Generates an interactive chart with configurable appearance and interaction controls.
+
+    Parameters:
+    - df (pd.DataFrame): The input DataFrame containing the data to be plotted.
+    - x (str): The column name in 'df' to be used for the x-axis of the plot.
+    - y (str): The column name in 'df' to be used for the y-axis of the plot.
+    - target (str): The column name in 'df' to be used for coloring the scatter plot points.
+
+    Returns:
+    - Chart: An Altair chart object representing the interactive scatter plot.
+
+    Features:
+    - The chart's title is dynamically generated based on the 'y', 'x', and 'target' values.
+    - The chart's background is gray and padding is added around the chart.
+    - The chart has interactive zoom and pan functionality:
+      - Zoom: Users can drag to create a rectangle to zoom into a specific area.
+      - Pan: Users can hold the 'alt' key and drag to pan across the chart.
+    - Axes and titles have customized font sizes and appearances.
+    - The chart's view size is set to a fixed width and height with a gray fill and no stroke.
+    """
 
     result = (Chart(df, title=f"{y} by {x} for {target}").
               mark_circle().encode(x=x, y=y, tooltip=df.columns.to_list(), color=target).interactive())
